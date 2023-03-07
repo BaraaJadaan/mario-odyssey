b2d6e2e67bc355f9787591c3d51e28ace9c0af67
BaraaJadaan <52387771+BaraaJadaan@users.noreply.github.com>
2023-03-08 02:39:34 +0300

    Finished Heading & Hero sections

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './Home/Header'
+import Hero from './Home/Hero'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Header/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
